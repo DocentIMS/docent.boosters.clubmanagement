8ebfcb1bf244fdb84d3724d201fbde216af44da8
Luke Brannon <luke.brannon@gmail.com>
2017-01-30 22:44:16 -0800

    added attendance record type and configured default title generation

--- src/docent/boosters/clubmanagement/content/attendance_record.py
+++ src/docent/boosters/clubmanagement/content/attendance_record.py
@@ -14,6 +14,7 @@ from zope import schema
 from zope.schema.vocabulary import SimpleVocabulary
 from zope.schema.interfaces import IVocabularyFactory
 from zope.interface import implementer
+from zope.interface import implements
 
 from docent.boosters.clubmanagement import _
 from docent.boosters.clubmanagement.content.bootser_club import IBoosterClub
@@ -22,10 +23,28 @@ from docent.boosters.clubmanagement.content.bootser_club import IBoosterClub
 logger = logging.getLogger("Plone")
 
 
+def computeTitle():
+    date_obj = date.today()
+    date_str = date_obj.strftime('%B %Y')
+
+    return u'%s' % date_str
+
 class IAttendanceRecord(form.Schema):
     """
     Uses IDublinCore
     """
+
+    title = schema.TextLine(
+        title=_(u"Meeting Date"),
+        required=True,
+        defaultFactory=computeTitle,
+    )
+
+    description = schema.Text(
+        title=_(u"Description"),
+        description=_(u"A summary of the meeting."),
+        required=False,
+    )
 
     clubs_present = schema.List(
         title=_(u'Clubs in Attendance'),
